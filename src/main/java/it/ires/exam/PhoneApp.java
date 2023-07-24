e11e5c8d8477f486a745dd32bc5bc772a907abc4
xWolf <buosi.matteo@gmail.com>
2023-07-24 12:50:52 +0200

    part 4 partially done pt2

--- src/main/java/it/ires/exam/PhoneApp.java
+++ src/main/java/it/ires/exam/PhoneApp.java
@@ -1,5 +1,7 @@
 package it.ires.exam;
 
+import it.ires.exam.exception.CantCreateSimException;
+import it.ires.exam.exception.CantCreateSmartphoneException;
 import it.ires.exam.exception.NumberNotValidException;
 import it.ires.exam.exception.RecievereAlreadyinACallException;
 
@@ -7,6 +9,12 @@ public class PhoneApp {
 
     public static void main(String[] args) {
         NetworkDistribution networkDistribution = new NetworkDistribution();
+        try {
+            networkDistribution.createNewSimCard("+391234567890",0.0,PhonePlan.SUPER);
+            networkDistribution.createNewSmartphone("+391234567890",0.0,"Matteo",0,PhonePlan.SUPER);
+        } catch (CantCreateSimException | CantCreateSmartphoneException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
